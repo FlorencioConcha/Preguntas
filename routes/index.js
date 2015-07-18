8e1e8cab86e92e78c676a2c723eb3a48adc2e177
FlorencioConcha <nonimof@gmail.com>
2015-07-18 22:14:00 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var quizControler = require('../control/quiz_controller');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
-
+router.get('quizes/question', quizControler.question);
+router.get('quizes/answer', quizControler.answer);
 module.exports = router;

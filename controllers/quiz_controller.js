2d5539543615967043f84d974d3b3409065ef555
FlorencioConcha <nonimof@gmail.com>
2015-07-27 00:33:36 +0200

    version2

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,7 +2,7 @@ exports.question = function(req,res){
   res.render('quizes/question',{pregunta:'Capital de Italia'});
 };
 exports.answer = function(req,res){
-  if (req.query.respuesta  === 'Roma'){
+  if (req.query.respuesta  == 'Roma'){
     res.render('quizes/answer', {respuesta: 'Correcto'});
   } else  {
     res.render('quizes/answer', {respuesta: 'Incorrecto'});

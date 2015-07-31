e920ee5771943ad87b8edf064f04a59fd29f9ca5
FlorencioConcha <nonimof@gmail.com>
2015-07-31 21:26:01 +0200

    ultima

--- models/models.js
+++ models/models.js
@@ -1,18 +1,18 @@
 var path = require('path');
-Var Sequelize =require ('sequelize');
+var Sequelize =require ('sequelize');
 var sequelize = new Sequelize(null,null,null,
                       {dialect: "sqlite",
                       storage: "quiz.sqlite"});
-var Quiz = sequelize.import(path.join(_dirname,'quiz'));
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz;
-  sequelize.sync().success(function(){
-    Quiz.count().success(function(count){
+  sequelize.sync().then(function(){
+    Quiz.count().then(function(count){
       if (count === 0){
         Quiz.create({
           pregunta: 'Capital de Italia',
           respuesta: 'Roma'
         })
-      .success(function (){console.log('Base de datos inicalizada')});
+      .then(function (){console.log('Base de datos inicalizada')});
     };
   });
 });
--- models/quiz.js
+++ models/quiz.js
@@ -1,4 +1,4 @@
-modules.exports = function(sequelize, DataTypes){
+module.exports = function(sequelize, DataTypes){
   return sequelize.define('Quiz',
         { pregunta: DataTypes.STRING,
           respuesta: DataTypes.STRING,
